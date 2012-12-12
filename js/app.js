a23f047724805aa04a896409552d776016876ba0
Dan Williams <me@deedubs.com>
2012-12-11 23:19:05 -0500

    progress from the HackNight!

--- js/app.js
+++ js/app.js
@@ -1,10 +1,68 @@
-angular
-  .module('image',[])
-  .controller('image', imageController)
-  .run(function () {
-    console.log('Run'); 
-  });
+var layouts = [
+  [
+    [4, 4, 4],
+    [6, 6],
+    [4, 4, 4]
+  ],
+  [
+    [6, 6],
+    [4, 4, 4]
+  ]
+];
 
-function imageController () {
-  
+angular
+  .module('image', [])
+  .directive('viewport', viewport)
+  .controller('image', imageController);
+
+function viewport () {
+  return {
+    controller: function($scope, $element) {
+      var el = $element;
+
+      el.bind('dragenter', function (e) {
+        console.log('enter');
+      });
+
+      el.bind('dragover', function (e) {
+
+      });
+
+      el.bind('dragleave', function (e) {
+        console.log('leave');
+      });
+
+      el.bind('dragend', function (e) {
+        console.log('end', e);
+      });
+      
+      el.bind('drop', function (e) {
+        console.log(e);
+        var image = e.originalEvent.dataTransfer.files[0]
+          , reader = new FileReader;
+        
+        e.preventDefault();
+
+        reader.onload = function(e) {
+          $scope.$apply(function() {
+            $scope.imageSrc = e.target.result;  
+          });
+        };
+
+        reader.readAsDataURL(image);
+        
+        return false;
+      });
+    }
+  }  
+}
+
+function imageController ($scope) {
+  $scope.layout = layouts[0];
+
+  $scope.changeLayout = function () {
+    $scope.layout = $scope.layout == layouts[0]
+      ? layouts[1]
+      : layouts[0]; 
+  }
 }
